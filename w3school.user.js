c60ff3da8c3a2af2133e4581785c14d9d3e4e560
halffunction <halffunction@yandex.com>
2020-12-01 18:29:44 +0530

    href.search, submitTryit(1) not working

--- w3school.user.js
+++ w3school.user.js
@@ -9,6 +9,24 @@
 // ==/UserScript==
 
 document.onkeyup = function(e) {
+    let href = document.querySelector(".w3-left.w3-btn").getAttribute("href");
+
+    if (href.search(/https:\/\/www.w3schools.com*/)) {
+        // when left arrow then go to previous chapter
+        if (e.which == 37) {
+            let href = document.querySelector(".w3-left.w3-btn").getAttribute("href");
+            window.location.assign(href)
+        }
+
+        // when right arrow type then
+        // transfer into next chapter
+        if (e.which == 39) {
+            let href = document.querySelector(".w3-right.w3-btn").getAttribute("href");
+            window.location.assign(href)
+        }
+
+    }
+
     // when you tyoe R it rotate screen
     if (e.altKey && e.which == 82) {
         restack(currentStack);
@@ -16,18 +34,5 @@ document.onkeyup = function(e) {
     // when type N it run program
     if (e.altKey && e.which == 78) {
         submitTryit(1);
-    }
-
-    // when left arrow then go to previous chapter
-    if (e.which == 37) {
-        let href = document.querySelector(".w3-left.w3-btn").getAttribute("href");
-        window.location.assign(href)
-    }
-
-    // when right arrow type then
-    // transfer into next chapter
-    if (e.which == 39) {
-        let href = document.querySelector(".w3-right.w3-btn").getAttribute("href");
-        window.location.assign(href)
     }
 }
